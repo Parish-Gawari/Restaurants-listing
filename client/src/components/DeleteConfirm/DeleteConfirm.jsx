dfd25fe1e0a5610078b49f0cc89c5987d2203e22
Samael-24 <parishk1024@gmail.com>
2023-11-17 10:08:22 +0530

    Added Delete Confirmation

--- client/src/components/DeleteConfirm/DeleteConfirm.jsx
+++ client/src/components/DeleteConfirm/DeleteConfirm.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-const DeleteConfirm = () => {
-  return <div>DeleteConfirm</div>;
+const DeleteConfirm = ({
+  confirm,
+  confrimToggle,
+  modalDeleteHandler,
+  currentId,
+}) => {
+  const handleClose = () => confrimToggle(!confirm);
+
+  return (
+    <>
+      <Modal show={confirm} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Do you really want to delete this?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button
+            variant="danger"
+            onClick={() => modalDeleteHandler(currentId)}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default DeleteConfirm;
--- client/src/components/RestaurantList/RestaurantList.jsx
+++ client/src/components/RestaurantList/RestaurantList.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { IoCallOutline } from "react-icons/io5";
 import { CiLocationOn } from "react-icons/ci";
+import DeleteConfirm from "../DeleteConfirm/DeleteConfirm";
 
 const RestaurantList = () => {
   const [toggle, setToggle] = useState(false);
+  const [currentId, setCurrentID] = useState("-1");
   const [list, setList] = useState();
   useEffect(() => {
     axios
@@ -17,47 +19,66 @@ const RestaurantList = () => {
 
   const deleteHandler = (id) => {
     setToggle(!toggle);
+    setCurrentID(id);
+    return;
+  };
+
+  const modalDeleteHandler = (id) => {
     axios
       .delete(`http://localhost:8090/crud/${id}`)
       .then((result) => {
         setToggle(!toggle);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setToggle(!toggle);
+      });
+    setCurrentID("-1");
   };
 
   const editHandler = () => {};
   return (
-    <div
-      className="d-flex flex-column flex-wrap flex-md-row p-4 gap-4  h-100 "
-      style={{ marginLeft: "1.5rem" }}>
-      {list &&
-        list.map((ele) => (
-          <div
-            className="border border-secondary rounded p-3"
-            key={ele.id}
-            style={{ width: "18em" }}>
-            <h5>{ele.name}</h5>
-            <div className="d-flex align-items-center gap-1 mb-2">
-              <IoCallOutline />
-              <span>{ele.contact}</span>
+    <>
+      {toggle && (
+        <DeleteConfirm
+          confirm={toggle}
+          confrimToggle={setToggle}
+          modalDeleteHandler={modalDeleteHandler}
+          currentId={currentId}
+        />
+      )}
+      <div
+        className="d-flex flex-column flex-wrap flex-md-row p-4 gap-4  h-100 "
+        style={{ marginLeft: "1.5rem" }}>
+        {list &&
+          list.map((ele) => (
+            <div
+              className="border border-secondary rounded p-3"
+              key={ele.id}
+              style={{ width: "18em" }}>
+              <h5>{ele.name}</h5>
+              <div className="d-flex align-items-center gap-1 mb-2">
+                <IoCallOutline />
+                <span>{ele.contact}</span>
+              </div>
+              <div className="d-flex align-items-center gap-1 mb-2 ">
+                <CiLocationOn />
+                <span>{ele.address}</span>
+              </div>
+              <div>
+                <button
+                  className="bg-danger text-white border-0 rounded px-2 py-1 me-2"
+                  onClick={() => deleteHandler(ele.id)}>
+                  Delete
+                </button>
+                <button className="bg-success text-white border-0 rounded px-2 py-1">
+                  Edit
+                </button>
+              </div>
             </div>
-            <div className="d-flex align-items-center gap-1 mb-2 ">
-              <CiLocationOn />
-              <span>{ele.address}</span>
-            </div>
-            <div>
-              <button
-                className="bg-danger text-white border-0 rounded px-2 py-1 me-2"
-                onClick={() => deleteHandler(ele.id)}>
-                Delete
-              </button>
-              <button className="bg-success text-white border-0 rounded px-2 py-1">
-                Edit
-              </button>
-            </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   );
 };
 
